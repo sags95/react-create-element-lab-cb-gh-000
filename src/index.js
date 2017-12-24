002a8bed5f0cbe8bee85715b70cd08aaa9b66294
Alex Sagel <alex.sagel7587@gmail.com>
2017-12-24 17:38:40 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // IMPORTANT!!!! uncomment the lines below to make your tests work
-/*
+const meInReact = React.createElement('div', {class: 'me'},
+
+)
+
+
+
 ReactDOM.render(
   meInReact,
   document.getElementById('global')
 );
 
 export default meInReact
-*/
